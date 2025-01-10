64234c9d3616ffeb716b7f3785dc3c2f2428dea3
Albert Garcia <91468433+1637521albert@users.noreply.github.com>
2025-01-10 12:02:07 +0100

    Add files via upload

---
+++ wp1.py
@@ -0,0 +1,27 @@
+import networkx as nx
+from synutility.SynIO.data_type import load_from_pickle
+from synutility.SynVis.graph_visualizer import GraphVisualizer
+import matplotlib.pyplot as plt
+
+def load_data(file_path):
+    data = load_from_pickle(file_path)
+    return data
+
+def extract_reaction_center(data):
+    G = data[0]['ITS']
+    reaction_center = nx.edge_subgraph(G,
+                                       [(e[0], e[1]) for e in G.edges(data=True)
+                                        if e[2]["standard_order"] != 0])
+    return reaction_center
+
+def plot(data, reaction_center):
+    fig, ax = plt.subplots(2, 1, figsize=(15, 10))
+    vis = GraphVisualizer()
+    vis.plot_its(data[0]['ITS'], ax[0], use_edge_color=True)
+    vis.plot_its(reaction_center, ax[1], use_edge_color=True)
+    plt.show()
+
+file_path = "Data/ITS_graphs.pkl.gz"
+data = load_data(file_path)
+reaction_center = extract_reaction_center(data)
+plot(data, reaction_center)
---
+++ wp2.py
@@ -0,0 +1,18 @@
+from wp1 import reaction_center
+from networkx import is_isomorphic
+
+def clustering(reactions):
+    partition = []
+    for reaction in reactions:
+        rc = reaction_center(reaction)
+        assigned = False
+        for q in partition:
+            if is_isomorphic(rc, q[0],
+                             node_match=lambda n1, n2: n1['charge'] == n2['charge'] and n1['element'] == n2['element'],
+                             edge_match=lambda e1, e2: e1['order'] == e2['order']):
+                q.append(rc)
+                assigned = True
+                break
+        if not assigned:
+            partition.append([rc])
+    return partition
