5d71360ebe0e5b72124cec2ee7fdaef4b5dbb3cc
jace <125557299+jacebrooklyn@users.noreply.github.com>
2025-01-14 22:23:43 +0100

    Update wp1.py

--- wp1.py
+++ wp1.py
@@ -8,7 +8,7 @@ def load_data(file_path):
     return data
 
 def extract_reaction_center(data):
-    G = data[0]['ITS']
+    G = data['ITS']
     reaction_center = nx.edge_subgraph(G,
                                        [(e[0], e[1]) for e in G.edges(data=True)
                                         if e[2]["standard_order"] != 0])
@@ -17,11 +17,13 @@ def extract_reaction_center(data):
 def plot(data, reaction_center):
     fig, ax = plt.subplots(2, 1, figsize=(15, 10))
     vis = GraphVisualizer()
-    vis.plot_its(data[0]['ITS'], ax[0], use_edge_color=True)
-    vis.plot_its(reaction_center, ax[1], use_edge_color=True)
-    plt.show()
+   
+    # Plot the ITS graph
+    vis.plot_its(data["ITS"], ax=ax[0], use_edge_color=True)
+    ax[0].set_title("ITS Graph")
 
-file_path = "Data/ITS_graphs.pkl.gz"
-data = load_data(file_path)
-reaction_center = extract_reaction_center(data)
-plot(data, reaction_center)
+    # Plot the Reaction Center
+    vis.plot_its(reaction_center, ax=ax[1], use_edge_color=True)
+    ax[1].set_title("Reaction Center")
+
+    plt.show()
