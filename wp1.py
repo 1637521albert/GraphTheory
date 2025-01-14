6dfe893baaa4f6d12e0244b0e608c9e926da6c3f
Albert Garcia <91468433+1637521albert@users.noreply.github.com>
2025-01-14 22:32:06 +0100

    Update wp1.py

--- wp1.py
+++ wp1.py
@@ -17,13 +17,11 @@ def extract_reaction_center(data):
 def plot(data, reaction_center):
     fig, ax = plt.subplots(2, 1, figsize=(15, 10))
     vis = GraphVisualizer()
-   
-    # Plot the ITS graph
-    vis.plot_its(data["ITS"], ax=ax[0], use_edge_color=True)
-    ax[0].set_title("ITS Graph")
-
-    # Plot the Reaction Center
-    vis.plot_its(reaction_center, ax=ax[1], use_edge_color=True)
-    ax[1].set_title("Reaction Center")
-
+    vis.plot_its(data['ITS'], ax[0], use_edge_color=True)
+    vis.plot_its(reaction_center, ax[1], use_edge_color=True)
     plt.show()
+
+# file_path = "Data/ITS_graphs.pkl.gz"
+# data = load_data(file_path)
+# reaction_center = extract_reaction_center(data[0])
+# plot(data, reaction_center)
